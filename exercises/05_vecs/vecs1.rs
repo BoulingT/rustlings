fed4de62193061940f08b70af0c404ef639ec9f1
Theo <119578352+BoulinguezTheo@users.noreply.github.com>
2024-06-01 16:48:33 +0200

    section 4 and 5 done

--- exercises/05_vecs/vecs1.rs
+++ exercises/05_vecs/vecs1.rs
@@ -7,13 +7,21 @@
 //
 // Execute `rustlings hint vecs1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     let a = [10, 20, 30, 40]; // a plain array
-    let v = // TODO: declare your vector here with the macro for vectors
+    /* let v = vec![10, 20, 30, 40]; */
+    let v = create_vector(&a);
 
     (a, v)
+}
+
+fn create_vector(array: &[i32]) -> Vec<i32> {
+    let mut vector: Vec<i32> = Vec::new();
+    for element in array {
+        vector.push(*element);
+    }
+    vector
 }
 
 #[cfg(test)]
